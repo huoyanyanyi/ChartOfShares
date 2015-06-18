11d04d51d049bfe1495fc2619dbea2d5b3b9d322
HaoQianji <569360886@qq.com>
2015-06-18 08:36:00 +0800

    Chart 0.0.3

--- Chart/src/com/chart/view/MyPanel.java
+++ Chart/src/com/chart/view/MyPanel.java
@@ -28,11 +28,19 @@ public class MyPanel extends JPanel {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
 		DataDao dataDao = new DataDao();
+		int y1 = 0;
+		int y2 = 0;
+		int y3 = 0;
+		int y4 = 0;
+		int y5 = 0;
+		int y6 = 0;
+		this.setBackground(new Color(10,10,10));
 		try {
-//			datas = dataDao.initDatas(MainView.code);
-			datas = dataDao.initDatas("000875");
+			datas = dataDao.initDatas(MainView.code);
+//			datas = dataDao.initDatas("000875");
 			java.util.Iterator<Data> it = datas.iterator();	
 			int numT = 0;
+			g.setColor(Color.WHITE);
 			g.drawLine((60-25)+5,450,(60-25)+5,440);
 			while (it.hasNext()) {
 				numT++;
@@ -42,7 +50,7 @@ public class MyPanel extends JPanel {
 				Data data = (Data)it.next();	
 				Date date = data.getDate();
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-				String dateT = sdf.format(data.getDate());
+				String dateT = sdf.format(date);
 				double open = data.getOpen();
 				double close = data.getClose();
 				int Close = (int)(close*35)-700;
@@ -56,6 +64,23 @@ public class MyPanel extends JPanel {
 				double lengthOfOP = data.getLengthOfOP();
 				int LengthOfOP = (int) (lengthOfOP*40);
 				
+				
+				
+				double ma5 = data.getMa5();
+				int Ma5 = (int)(ma5*35)-700;
+				y1 = y2;
+				y2 = Ma5;
+				
+				double ma10 = data.getMa10();
+				int Ma10 = (int)(ma10*35)-700;
+				y3 = y4;
+				y4 = Ma10;
+				
+				double ma20 = data.getMa20();
+				int Ma20 = (int)(ma20*35)-700;
+				y5 = y6;
+				y6 = Ma20;
+				
 				double volume = data.getVolume();	
 				
 				int Volume = (int)volume/100000*2;
@@ -63,7 +88,17 @@ public class MyPanel extends JPanel {
 				System.out.println(Open);
 				System.out.println(LengthOfHL);
 				System.out.println(LengthOfOP);
-
+				
+				if(numT!=1){
+					g.setColor(Color.YELLOW);
+					g.drawLine((60+(numT-2)*25)+5,y1, 60+(numT-1)*25+5, y2);
+					
+					g.setColor(Color.GREEN);
+					g.drawLine((60+(numT-2)*25)+5,y3, 60+(numT-1)*25+5, y4);
+					
+					g.setColor(Color.ORANGE);
+					g.drawLine((60+(numT-2)*25)+5,y5, 60+(numT-1)*25+5, y6);
+				}
 				
 				if (open>=close){
 					g.setColor(Color.RED);
@@ -72,7 +107,7 @@ public class MyPanel extends JPanel {
 					g.fillRect((60+(numT-1)*25), 420-Volume, 10, Volume);
 					
 					g.drawLine((60+(numT-1)*25)+5,Low, 60+(numT-1)*25+5, High);
-					g.setColor(Color.BLACK);
+					g.setColor(Color.WHITE);
 					if(numT%5 == 0){
 						g.drawString(dateT,35+(numT-1)*25-5,470);
 						g.drawLine((60+(numT-1)*25)+5,450,(60+(numT-1)*25)+5,440);
@@ -88,7 +123,7 @@ public class MyPanel extends JPanel {
 					g.fillRect((60+(numT-1)*25), Close, 10, LengthOfOP);
 					g.fillRect((60+(numT-1)*25), 420-Volume, 10, Volume);
 					g.drawLine((60+(numT-1)*25)+5,Low, 60+(numT-1)*25+5, High);
-					g.setColor(Color.BLACK);
+					g.setColor(Color.WHITE);
 					if(numT%5 == 0){
 						g.drawString(dateT,35+(numT-1)*25-5,470);
 						g.drawLine((60+(numT-1)*25)+5,450,(60+(numT-1)*25)+5,440);
@@ -118,7 +153,7 @@ public class MyPanel extends JPanel {
 			
 		}
 		finally{
-			g.setColor(Color.BLACK);
+			g.setColor(Color.WHITE);
 			g.drawLine(24, 450, 24, 24);
 			g.drawLine(24, 450, 780, 450);
 		//	g.drawRect(0, 0, 100, 100);
